a6fee7d4ddcb94c40f1d9c9ec6c51ffd444c9f41
Tawdi <ahmedtawdi00@gmail.com>
2025-02-28 16:21:58 +0000

    ajoute de Transactions Controller

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+       return view('dashboard.transactions');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+           'transaction_date' => ['required', 'date'],
+            'description' => ['nullable', 'string', 'max:255'],
+            'type' => ['required', 'in:expense,income'], 
+            'amount' => ['required', 'numeric', 'min:0'],
+            'category_id' => ['required', 'exists:categories,id'],
+        ]);
+        $validated['profile_id']=  session('current_profile_id');
+        // dd($validated);
+        Transaction::create($validated);
+        return redirect()->route('transactions.index')->with('success', 'Transaction ajouté avec succès !');
+        
+    }
+    
+    /**
+     * Display the specified resource.
+     */
+    public function show(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Transaction $transaction)
+    {
+        //
+    }
+}
