56ba5db5c473166cf95a269eb633a543fa9ab8bd
Tawdi <ahmedtawdi00@gmail.com>
2025-03-07 14:28:50 +0000

    crud pour goals

--- app/Http/Controllers/SavingsGoalController.php
+++ app/Http/Controllers/SavingsGoalController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\SavingsGoal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SavingsGoalController extends Controller
 {
@@ -11,8 +12,10 @@ class SavingsGoalController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        return view('dashboard.goals');
+    { 
+        $goals = Auth::user()->savingsGoals;
+        // dd($goals);
+        return view('dashboard.goals',compact('goals'));
     }
 
     /**
@@ -29,6 +32,16 @@ class SavingsGoalController extends Controller
     public function store(Request $request)
     {
         //
+        // dd($request);
+        $validated = $request->validate([
+            'title'=> ['required', 'string', 'max:255'],
+            'target_amount'=> ['required', 'numeric', 'gt:0'],
+            'deadline'=> ['nullable','date','after:today'] 
+        ]);
+        // dd($validated);
+        $validated['user_id']= Auth::user()->id;
+        SavingsGoal::create($validated);
+        return redirect()->route('goals.index')->with('success','Objectif créé avec succès !');
     }
 
     /**
@@ -42,24 +55,38 @@ class SavingsGoalController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(SavingsGoal $savingsGoal)
+    public function edit($savingsGoal)
     {
-        //
+        $savingsGoal = SavingsGoal::findOrFail($savingsGoal);
+
+        return response()->json($savingsGoal);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, SavingsGoal $savingsGoal)
+    public function update(Request $request,  $savingsGoal)
     {
-        //
+        $validated = $request->validate([
+            'title'=> ['required', 'string', 'max:255'],
+            'target_amount'=> ['required', 'numeric', 'gt:0'],
+            'current_amount'=> ['required', 'numeric'],
+            'deadline'=> ['nullable','date','after:today'] 
+        ]);
+        $goal = SavingsGoal::findOrFail($savingsGoal);
+        // dd($validated);
+        $goal->update($validated);
+
+        return redirect()->route('goals.index')->with('success','Objectif modifié avec succès !');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(SavingsGoal $savingsGoal)
+    public function destroy( $savingsGoal)
     {
-        //
+        $goal = SavingsGoal::findOrFail($savingsGoal);
+        $goal->delete();
+        return redirect()->route('goals.index')->with('success','Objectif supprimé avec succès !');
     }
 }
